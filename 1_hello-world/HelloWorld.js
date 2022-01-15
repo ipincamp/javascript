504cb426bce97f512fafdbc20558db4d5d6f0beb
ipincamp <arif.lngsrn@gmail.com>
2022-01-15 17:03:26 +0700

    hello world

---
+++ 1_hello-world/HelloWorld.js
@@ -0,0 +1,10 @@
+/**
+ * Ini merupakan komentar atau comment.
+ *
+ * tipe ini untuk beberapa baris atau multiline.
+ */
+
+// Ini juga komentar, namun 1 baris saja
+
+// Inspect dan pergi ke konsole untuk melihat hasilnya.
+console.log("Hello world");
