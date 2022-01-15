05027bf978954c4703f75cd10b804c4859e83f97
ipincamp <70874414+ipincamp@users.noreply.github.com>
2022-01-15 20:35:20 +0700

    Type Variables

---
+++ 2_variables/Variables.js
@@ -0,0 +1,24 @@
+/**
+ * Variable di JavaScript
+ * ada 3 jenis:
+ * 
+ * var - Sudah ditinggalkan
+ *     > Digunakan di semua kode JavaScript dari tahun 1995 - 2015
+ *     > Sebagai gantinya, gunakan variable let dan const
+ * 
+ * let - Tipe variable yang bisa dirubah nilainya
+ * const - Konstant tidak bisa dirubah nilainya
+ * 
+ */
+var nameVar = "value";
+
+/**
+ * Aturan umum untuk membangun nama untuk variabel (pengidentifikasi unik) adalah:
+ * 
+ * Nama dapat berisi huruf, angka, garis bawah, dan tanda dolar.
+ * Nama harus diawali dengan huruf
+ * Nama juga dapat dimulai dengan $ dan _ (tetapi kami tidak akan menggunakannya dalam tutorial ini)
+ * Nama peka huruf besar/kecil (y dan Y adalah variabel yang berbeda)
+ * Kata-kata yang dicadangkan (seperti kata kunci JavaScript) tidak dapat digunakan sebagai nama
+ * 
+ */
