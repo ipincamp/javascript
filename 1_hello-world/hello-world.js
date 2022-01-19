b348234c7fd2c6de90f0e7b35860b777cb6d226d
ipincamp <arif.lngsrn@gmail.com>
2022-01-19 09:06:48 +0700

    re style module

---
+++ 1_hello-world/hello-world.js
@@ -0,0 +1,18 @@
+/**
+ * Ini merupakan komentar atau comment.
+ *
+ * tipe ini untuk beberapa baris atau multiline.
+ */
+
+// Ini juga komentar, namun 1 baris saja
+
+console.log("Hello world");
+// Inspect dan pergi ke konsole untuk melihat hasilnya.
+
+/**
+ * Titik Koma
+ * 
+ * Setiap akhiran kode program JavaScript harus konsisten
+ * terhadap titik koma. Sifatnya tidak wajib, jadi boleh
+ * menggunakan titik koma maupun tidak menggunakannya.
+ */
--- 2_variables/Variables.js
+++ 2_variables/Variables.js
@@ -1,24 +1,26 @@
 /**
- * Variable di JavaScript
+ * Tempat untuk menyimpan sebuah data.
+ * 
+ * Variabel di JavaScript
  * ada 3 jenis:
- * 
  * var - Sudah ditinggalkan
- *     > Digunakan di semua kode JavaScript dari tahun 1995 - 2015
- *     > Sebagai gantinya, gunakan variable let dan const
- * 
- * let - Tipe variable yang bisa dirubah nilainya
+ * > Digunakan di semua kode JavaScript dari tahun 1995-2015
+ * > Sebagai gantinya, gunakan variabel let dan const
+ * let - Tipe variabel yang bisa dirubah nilainya
  * const - Konstant tidak bisa dirubah nilainya
  * 
  */
-var nameVar = "value";
+// Contoh pembuatan variabel
+let nameVar = "value";
 
 /**
- * Aturan umum untuk membangun nama untuk variabel (pengidentifikasi unik) adalah:
+ * Aturan umum untuk membangun nama untuk variabel adalah:
  * 
- * Nama dapat berisi huruf, angka, garis bawah, dan tanda dolar.
- * Nama harus diawali dengan huruf
- * Nama juga dapat dimulai dengan $ dan _ (tetapi kami tidak akan menggunakannya dalam tutorial ini)
- * Nama peka huruf besar/kecil (y dan Y adalah variabel yang berbeda)
- * Kata-kata yang dicadangkan (seperti kata kunci JavaScript) tidak dapat digunakan sebagai nama
+ * > Nama dapat berisi huruf, angka, garis bawah, dan tanda dolar.
+ * > Nama harus diawali dengan huruf
+ * > Nama juga dapat dimulai dengan $ dan _
+ * > Nama peka huruf besar/kecil (y & Y itu variabel yang berbeda)
+ * > Kata-kata yang dicadangkan (seperti let, const, var)
+ *   tidak dapat digunakan sebagai nama
  * 
  */
