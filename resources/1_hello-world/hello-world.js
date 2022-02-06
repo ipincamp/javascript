c8a527729875960fb2dd24332883db4d9c41d3fb
ipincamp <arif.lngsrn@gmail.com>
2022-02-06 13:20:05 +0700

    refactor

---
+++ resources/1_hello-world/hello-world.js
@@ -0,0 +1,59 @@
+/**
+ * Ini merupakan komentar atau comment.
+ *
+ * Komentar ini digunakan untuk beberapa
+ * baris seperti ini
+ */
+
+// Ini komentar satu baris
+
+/**
+ * Titik Koma (;)
+ * 
+ * Setiap akhiran kode program JavaScript harus konsisten
+ * terhadap titik koma. Sifatnya tidak wajib, jadi boleh
+ * menggunakan titik koma maupun tidak menggunakannya.
+ * 
+ */
+
+/**
+ * Escape Sequence
+ * 
+ * \n menjadi ENTER
+ * \t menjadi TAB
+ * \' menjadi '
+ * \" menjadi "
+ * \* menjadi *
+ * \\ menjadi \
+ * 
+ */
+
+/**
+ * Petik Dua
+ * 
+ * Ada 2 jenis format petik dua
+ *  Double Quotes (" ")
+ *  Single Quotes (' ')
+ * 
+ * Harus konsisten!
+ * 
+ */
+
+/**
+ * Console
+ * 
+ * Ada 6 tipe level console
+ *  debug   detail
+ *  info    informasi
+ *  log     sama seperti info
+ *  table   bentuk table
+ *  warn    informasi peringatan
+ *  error   informasi error
+ * 
+ */
+
+// Inspect dan pergi ke konsole untuk melihat hasilnya.
+console.log("Hello World");
+
+// Jika ingin dimunculkan ke HTML, gunakan cara ini
+document.writeln("<p>Hello World</p>")
